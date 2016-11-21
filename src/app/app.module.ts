141a3db0b270758e8cb06f9ae70755a189fb0333
bjsawyer <bjsawyer@vt.edu>
2016-11-20 21:48:49 -0500

    Added ProductComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProductListingComponent } from './product-listing/product-listing.component';
+import { ProductComponent } from './product/product.component';
 const routes: Routes = [
   { path: '', redirectTo: 'productListing', pathMatch: 'full' },
-  { path: 'productListing', component: ProductListingComponent }
+  { path: 'productListing', component: ProductListingComponent },
+  { path: 'selectedProduct/:id', component: ProductComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProductListingComponent
+    ProductListingComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
