02ba55f131fabd0ee43bfd532ebdf21787dbc344
bjsawyer <bjsawyer@vt.edu>
2016-11-20 21:49:17 -0500

    Hooked up router for click event on ProductListComponent

--- src/app/models/product/product.component.ts
+++ src/app/models/product/product.component.ts
@@ -1,8 +1,10 @@
 export class Product {
+  id: number;
   name: string;
   cost: number;
 
-  constructor(name: string, cost: number) {
+  constructor(id: number, name: string, cost: number) {
+    this.id = id;
     this.name = name;
     this.cost = cost;
   }
--- src/app/product-listing/product-listing.component.ts
+++ src/app/product-listing/product-listing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product/product.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-listing',
@@ -8,14 +9,18 @@ import { Product } from '../models/product/product.component';
 })
 export class ProductListingComponent implements OnInit {
   products = [
-    new Product('Reese\'s', 1.25),
-    new Product('Snickers', 1.50),
-    new Product('Milky Way', 1.25)
+    new Product(1, 'Reese\'s', 1.25),
+    new Product(2, 'Snickers', 1.50),
+    new Product(3, 'Milky Way', 1.25)
   ];
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  onSelect(product: Product) {
+    this.router.navigate(['/selectedProduct', product.id]);
+  }
 }
