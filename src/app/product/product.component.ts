991f968eb46a95f6655211c7c171e576bcfd1406
bjsawyer <bjsawyer@vt.edu>
2016-11-22 17:37:20 -0500

    Added product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product/product.component';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+  selectedProduct: Product = {
+    id: 1,
+    name: 'Test Product',
+    cost: 1.25
+  };
 
-  constructor() { }
+  currentBalance: number;
+
+  constructor() {
+    this.currentBalance = 0.00;
+   }
 
   ngOnInit() {
   }
 
+  addQuarter() {
+    this.currentBalance += 0.25;
+  }
+
+  addDollar() {
+    this.currentBalance += 1.00;
+  }
 }
