a1ab7636925defa56d042c269e2c9bf51d28d3b7
bjsawyer <bjsawyer@vt.edu>
2016-11-22 17:37:01 -0500

    Hooked up service in product-listing.component

--- src/app/product-listing/product-listing.component.ts
+++ src/app/product-listing/product-listing.component.ts
@@ -2,22 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product/product.component';
 import { Router } from '@angular/router';
 
+import { ProductService } from '../services/product.service';
+
 @Component({
   selector: 'app-product-listing',
   templateUrl: './product-listing.component.html',
   styleUrls: ['./product-listing.component.css']
 })
 export class ProductListingComponent implements OnInit {
-  products = [
-    new Product(1, 'Reese\'s', 1.25),
-    new Product(2, 'Snickers', 1.50),
-    new Product(3, 'Milky Way', 1.25)
-  ];
+  private products: Product[];
 
-  constructor(private router: Router) {
-  }
+  constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.productService.getProductListing().then(products => this.products = products);
   }
 
   onSelect(product: Product) {
