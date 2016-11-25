8fada6ffdf95fd9af38732f628db6e4b31998d4a
bjsawyer <bjsawyer@vt.edu>
2016-11-24 22:17:46 -0500

    Routing for AddProductComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProductListingComponent } from './product-listing/product-listing.component';
 import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'productListing', pathMatch: 'full' },
   { path: 'productListing', component: ProductListingComponent },
-  { path: 'selectedProduct/:id', component: ProductComponent }
+  { path: 'selectedProduct/:id', component: ProductComponent },
+  { path: 'addProduct', component: AddProductComponent }
 ];
 
 @NgModule({
