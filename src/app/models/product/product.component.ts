161bb152f638c39b7bcb8e55958b06fcafb82882
bjsawyer <bjsawyer@vt.edu>
2016-11-24 22:22:07 -0500

    Updates to ProductComponent

--- src/app/models/product/product.component.ts
+++ src/app/models/product/product.component.ts
@@ -2,10 +2,12 @@ export class Product {
   id: number;
   name: string;
   cost: number;
+  quantity: number;
 
-  constructor(id: number, name: string, cost: number) {
+  constructor(id: number, name: string, cost: number, quantity: number) {
     this.id = id;
     this.name = name;
     this.cost = cost;
+    this.quantity = quantity;
   }
 }
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/switchMap';
+
 import { Product } from '../models/product/product.component';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product',
@@ -7,26 +12,46 @@ import { Product } from '../models/product/product.component';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-  selectedProduct: Product = {
-    id: 1,
-    name: 'Test Product',
-    cost: 1.25
-  };
-
+  selectedProduct: Product;
   currentBalance: number;
+  id: number;
 
-  constructor() {
-    this.currentBalance = 0.00;
-   }
+  private subscription: any;
+
+  constructor(private productService: ProductService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit() {
+    let self = this;
+    this.currentBalance = 0.00;
+    this.subscription = this.route.params.subscribe(params => this.id = +params['id']);
+    this.productService.getProductById(this.id).then(product => self.selectedProduct = product);
   }
 
-  addQuarter() {
+  addNickel(): void {
+    this.currentBalance += 0.05;
+  }
+
+  addDime(): void {
+    this.currentBalance += 0.10;
+  }
+
+  addQuarter(): void {
     this.currentBalance += 0.25;
   }
 
-  addDollar() {
+  addDollar(): void {
     this.currentBalance += 1.00;
+  }
+
+  resetBalance(): void {
+    this.currentBalance = 0;
+  }
+
+  colorBalance(): Object {
+    let isExactBalance = this.selectedProduct && this.currentBalance === this.selectedProduct.cost;
+    let style = {
+      'color': isExactBalance ? '#33CC33' : 'red'
+    };
+    return style;
   }
 }
