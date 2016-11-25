4501f35e91ec9bdbbfb48a67e31b3dc7f3704ada
bjsawyer <bjsawyer@vt.edu>
2016-11-24 23:59:56 -0500

    Adding button to route back to ProductListingComponent on navbar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../../models/product/product.component';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() {
-  }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  goToProductListing(): void {
+    this.router.navigate(['/productListing']);
   }
 
 }
