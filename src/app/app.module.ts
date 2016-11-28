83440f06bc6d3e03cea488dd1ad12c91804de816
bjsawyer <bjsawyer@vt.edu>
2016-11-27 22:11:25 -0500

    Reorganizing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { ProductListingComponent } from './product-listing/product-listing.component';
 import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { AddProductFormComponent } from './add-product-form/add-product-form.component';
 import { ProductService } from './services/product.service';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
 import { InMemoryProductService } from './services/in-memory-product.service';
-import { AddProductComponent } from './add-product/add-product.component';
-import { AddProductFormComponent } from './add-product-form/add-product-form.component';
 
 @NgModule({
   declarations: [
